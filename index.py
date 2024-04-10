5d6f85211265d774b2bd8d40609fd6b66f14a689
DavidSheltonSF <david.faria012@gmail.com>
2024-04-10 17:43:27 -0300

    initial commit

---
+++ index.py
@@ -0,0 +1,7 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv20, MaxPooling20, Flatten, Dense, DropountP
